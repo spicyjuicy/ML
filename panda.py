feaba342b1e9f44a176a7e9103d8cfee898164a6
Max Kerslake <mkerslake@thunderhead.com>
2019-01-16 11:33:39 +0000

    third commit

--- panda.py
+++ panda.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 class Stock:
-    
+
     def __init__(self, file):
         self.file = file
-        self.df2 = pd.read_csv(self.file)
+        self.ready_up()
+        self.split(5)
 
     def ready_up(self):
-        df = self.df2
-       # df = pd.read_csv(self.file)
+        
+        df = pd.read_csv(self.file)
 
         df['Day_Name'] = pd.to_datetime(df['Date'])
         print(df['Date'].iloc[0],df['Date'].iloc[-1])
@@ -35,15 +37,19 @@ class Stock:
         df = df[df.major_index != 6]
         df = df[df.major_index != 0]
 
-        df = df.drop(columns=['Day_Name','DayNum','WeekNum','YearNum','rank'])
+        df = df.drop(columns=['Day_Name','DayNum','YearNum','rank'])
 
         for x in range(5):
             if df.major_index.iloc[-1] != 5:
                 df = df[:-1]
 
-       # df['Next Close'] = df.Close.shift(-4)
-
         self.df2 = df
+
+    def split(self,split_size):
+        print(len(self.df2))
+
+        self.arrs = np.split(self.df2.values,len(self.df2)/5)        
+        
 
     def print_head(self,size):
 
@@ -55,10 +61,16 @@ class Stock:
         print(self.df2.tail(size))
         print(self.df2.dtypes,len(self.df2.columns))
 
-        
+   
+
+      
 
 
 df = Stock('FB.csv')
-df.ready_up()
-df.print_head(20)
+print(len(df.df2))
+print(df.arrs[0])
+#print(df.arrs)
+print(np.shape(df.arrs))
+#df.print_head(20)
+#df.print_tail(20)
 #df.print_tail(20)
